715c4a1a221890d4db85a02b0204331abaff8486
Derek <derekdeming17@gmail.com>
2023-07-17 10:12:03 -0600

    time and space complexity solutions added

--- linkedLists/SinglyLinkedList/exercises/remove_dup_SLL.py
+++ linkedLists/SinglyLinkedList/exercises/remove_dup_SLL.py
@@ -25,6 +25,24 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+# ---------- ANOTHER SOLUTION ----------
+    def remove_duplicates(self):
+        if not self.head: 
+            return "Linked list is empty."
+        node_values = set()
+        current_node = self.head
+        prev_node = None
+        while current_node: 
+            if current_node.value in node_values:
+                prev_node.next = current_node.next
+                if current_node.next is None:
+                    self.tail = prev_node
+            else: 
+                node_values.add(current_node.value)
+                prev_node = current_node
+            current_node = current_node.next
+            return f"Duplicates removed. The list now has {len(node_values)} unique values: {node_values}"
+
 # ---------- ONE SOLUTION ----------
     def remove_duplicates(self): 
         if self.head is None: 
@@ -61,16 +79,28 @@ class LinkedList:
 
 
 
-
 # ------------------ Code Explanation ------------------
 '''
+if current_node.next.value in node_values -- Checks if the value of the next node is in the 
+node_values set, which would mean it's a duplicate.
 
+current_node.next = current_node.next.next -- If the value is a duplicate, this line "skips" 
+the next node, effectively removing it from the linked list by adjusting the next pointer of the 
+current node to point to the node after the next one.
 
 '''
 
 
 # ------------------ Time and Space Complexity Explanation ------------------
 '''
+Time Complexity - O(n) -- The algorithm traverses the linked list once, so the time complexity
+Overall, the time complexity of the remove_duplicates method is O(n), where n is the number 
+of nodes in the linked list. This is because the while loop iterates through the list once, 
+performing constant-time operations in each iteration.
 
+Space Complexity - O(1) -- may depend on which solution you take 
+The space complexity of the remove_duplicates method is O(1), as the space used does not grow 
+with the size of the input. It only requires a constant amount of extra space to store the 
+set node_values.
 
 '''
